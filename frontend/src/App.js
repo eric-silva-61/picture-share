0a737c7b27529c64f4c9770638e35b025239ca49
Eric Silva <62892917+eric-silva-61@users.noreply.github.com>
2021-08-25 15:06:49 -0600

    Add login/signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import NewPlace from './places/pages/NewPlace';
 import Users from './user/pages/Users';
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
 
 const App = () => {
   return (
@@ -29,6 +30,9 @@ const App = () => {
           </Route>
           <Route path="/places/:placeId" exact>
             <UpdatePlace />
+          </Route>
+          <Route to="/auth" exact>
+            <Auth />
           </Route>
           <Redirect to="/" />
         </Switch>
---
+++ frontend/src/user/pages/Auth.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import Card from '../../shared/components/UIElements/Card';
+import { useForm } from '../../shared/hooks/form-hooks';
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE
+} from '../../shared/util/validators';
+import './Auth.css';
+
+const Auth = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      email: {
+        value: '',
+        isValid: false
+      },
+      password: {
+        value: '',
+        isValid: false
+      }
+    },
+    false
+  );
+
+  const authSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
+  const switchModeHandler = () => {
+    if (!isLogin) {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: undefined
+        },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: {
+            value: '',
+            isValid: false
+          }
+        },
+        false
+      );
+    }
+
+    setIsLogin((prevMode) => !prevMode);
+  };
+
+  return (
+    <Card className="authentication">
+      <h2>Login Required</h2>
+      <hr />
+      <form onSubmit={authSubmitHandler}>
+        {!isLogin && (
+          <Input
+            id="name"
+            element="input"
+            type="text"
+            label="Name"
+            errorText="Please enter valid name"
+            onInput={inputHandler}
+            validators={[VALIDATOR_REQUIRE()]}
+          />
+        )}
+        <Input
+          id="email"
+          element="input"
+          type="email"
+          label="E-mail"
+          errorText="Please enter valid email address"
+          onInput={inputHandler}
+          validators={[VALIDATOR_EMAIL()]}
+        />
+        <Input
+          id="password"
+          element="input"
+          type="password"
+          label="Password"
+          errorText="Please enter valid password (minimum length 5)"
+          onInput={inputHandler}
+          validators={[VALIDATOR_MINLENGTH(5)]}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          {isLogin ? 'LOGIN' : 'SIGNUP'}
+        </Button>
+        <Button type="button" inverse onClick={switchModeHandler}>
+          SWITCH TO {isLogin ? 'SIGNUP' : 'LOGIN'}
+        </Button>
+      </form>
+    </Card>
+  );
+};
+
+export default Auth;
