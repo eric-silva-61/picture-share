5bf774c2c0e50e4575605b028c8b6148f740f3f0
Eric Silva <62892917+eric-silva-61@users.noreply.github.com>
2021-08-25 06:43:47 -0600

    Added top and side(mobile) navigation

--- frontend/src/user/components/UserItem.js
+++ frontend/src/user/components/UserItem.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Avatar from '../../shared/components/UIElements/Avatar';
+import Card from '../../shared/components/UIElements/Card';
 import './UserItem.css';
 
 const UserItem = (props) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={props.user.image} alt={props.user.name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{props.user.name}</h2>
-          <h3>
-            {props.user.places} {props.user.places === 1 ? 'Place' : 'Places'}
-          </h3>
-        </div>
-      </div>
+      <Card className="user-item__content">
+        <Link to={`/${props.user.id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={props.user.image} alt={props.user.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.user.name}</h2>
+            <h3>
+              {props.user.places} {props.user.places === 1 ? 'Place' : 'Places'}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 };
