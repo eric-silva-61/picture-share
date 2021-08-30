caacdbaaa67fcf996f2b59ec867ec7148e5e5c07
Eric Silva <62892917+eric-silva-61@users.noreply.github.com>
2021-08-30 12:47:36 -0600

    Auth middleware added to handle tokens and protect routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -3,17 +3,15 @@ const { check } = require('express-validator');
 
 const controllers = require('../controllers/places-controllers');
 const fileUpload = require('../middleware/file-upload');
+const checkAuth = require('../middleware/check-auth');
 
 const router = Router();
 
 router.get('/user/:id', controllers.getUserPlaces);
 router.get('/:id', controllers.getPlaceById);
-router.delete('/:id', controllers.deletePlace);
-router.patch(
-  '/:id',
-  [check('title').not().isEmpty(), check('description').isLength({ min: 5 })],
-  controllers.updatePlace
-);
+
+router.use(checkAuth);
+
 router.post(
   '/',
   fileUpload.single('image'),
@@ -24,5 +22,13 @@ router.post(
   ],
   controllers.createPlace
 );
+
+router.patch(
+  '/:id',
+  [check('title').not().isEmpty(), check('description').isLength({ min: 5 })],
+  controllers.updatePlace
+);
+
+router.delete('/:id', controllers.deletePlace);
 
 module.exports = router;
