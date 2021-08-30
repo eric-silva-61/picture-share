48e2ee5ae674350667a84ac68af153ce5e9983d7
Eric Silva <62892917+eric-silva-61@users.noreply.github.com>
2021-08-30 10:01:53 -0600

    Added multer fileupload to backend

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -2,12 +2,14 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const controllers = require('../controllers/users-controllers');
+const fileUpload = require('../middleware/file-upload');
 
 const router = Router();
 
 router.get('/', controllers.getUsers);
 router.post(
   '/signup',
+  fileUpload.single('image'),
   [
     check('email').normalizeEmail().isEmail(),
     check('name').not().isEmpty(),
